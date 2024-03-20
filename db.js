f923b46a5df1871b6d7ebb8cdc54a0e39b5cad0e
Ibnu Nasrulloh <ibnunasrulloh567@gmail.com>
2024-03-20 19:21:38 +0700

    [update api]

--- db.js
+++ db.js
@@ -6,15 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'quiz',
-  password: 'ibnu161206',
-  port: 5432,
-});
 
 async function connect() {
   try {
@@ -25,77 +18,104 @@ async function connect() {
   }
 }
 
-async function init() {
-  await connect();
+async function init()   {
+  await connect();  
 }
 
-
-
-app.get('/api/QuizzAnime', async (_req, res) => {
+app.get('/api/quizCategory/:categoryId', async (req, res) => {
   try {
-    console.log("quiz di hit");
-    const quizQuery = 'SELECT * FROM quiz';
-    const questionQuery = 'SELECT * FROM question';
-
-    const quizResult = await client.query(quizQuery);
-    const questionResult = await client.query(questionQuery);
-
-    const quizzes = quizResult.rows.map((quiz) => ({
-      name: quiz.name,
-      questions: questionResult.rows
-        .filter((question) => question.quiz_id === quiz.id)
-        .map((question) => ({
-          question: question.question_text,
-          options: question.options,
-          answer: question.answer,
-        })),
-    }));
-
-    res.json({ data: quizzes });
-  } catch (error) {
-    console.log('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
-app.post('/api/scores', async (req, res) => {
-  try {
-    const { user_id, quiz_name, score, name } = req.body;
-
-    const insertScoreQuery = `
-      INSERT INTO scores (user_id, quiz_name, score, name)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *;
+    console.log("dihit")
+    const categoryId = parseInt(req.params.categoryId, 10); 
+    
+    if (isNaN(categoryId)) {
+      res.status(400).json({ error: 'Invalid categoryId' });
+      return;
+    }
+    
+    const query = `
+      SELECT *
+      FROM quiz_kategori INNER JOIN quiz ON quiz_kategori.id = quiz.kategori_id WHERE quiz.kategori_id = $1;
     `;
 
-    const values = [user_id, quiz_name, score, name];
-    const result = await client.query(insertScoreQuery, values);
-
-    res.status(201).json({ data: result.rows[0] });
-  } catch (error) {
-    console.log('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error', message: error.message });
-  }
-});
-
-
-app.get('/api/leaderboard', async (_req, res) => {
-  try {
-    console.log("quizz di hit");
-    const scoresQuery = `
-      SELECT * FROM scores;
-    `;
-    const scoresResult = await client.query(scoresQuery);
-    const scoresData = scoresResult.rows;
-
-    res.json({ data: scoresData });
+    const { rows } = await client.query(query, [categoryId]);
+    res.json(rows);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.get('/api/quizs/:quizId', async (req, res) => {
+  try {
+    const { quizId } = req.params;
+    
+    if (isNaN(quizId)) {
+      res.status(400).json({ error: 'Invalid quizId' });
+      return;
+    }
+    
+    const query = `
+      SELECT *
+      FROM quiz INNER JOIN question ON quiz.id = question.quiz_id WHERE quiz.id = $1;
+    `;
+
+    const { rows } = await client.query(query, [quizId]);
+    res.json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/leaderboard/:leaderboardId', async (req, res) => {
+  try {
+    const { leaderboardId } = req.params;
+
+    if (isNaN(leaderboardId)) {
+      res.status(400).json({ error: 'Invalid leaderboardId' });
+      return;
+    }
+
+    const query = `
+      SELECT s.name , q.name AS quiz_name, MAX(s.score) AS score
+FROM scores s
+INNER JOIN quiz q ON s.quiz_id = q.id
+WHERE q.kategori_id = $1
+GROUP BY s.name, q.name 
+order by score desc;
+`; 
+
+    const { rows } = await client.query(query, [leaderboardId]);
+    res.json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+  app.post('/api/scores', async (req, res) => {
+    try {
+      const { user_id, quiz_id, score, name } = req.body;
+
+      const insertScoreQuery = `
+        INSERT INTO scores (user_id, quiz_id, score, name)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
+      `;
+
+      const values = [user_id, quiz_id, score, name];
+      const result = await client.query(insertScoreQuery, values);
+
+      res.status(201).json({ data: result.rows[0] });
+    } catch (error) {
+      console.log('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    }
+  });
+
+  
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -104,4 +124,3 @@ app.listen(PORT, () => {
 init();
 
 
-// agregat query
